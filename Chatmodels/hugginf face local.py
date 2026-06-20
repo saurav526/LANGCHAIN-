ed75934d3df46df0c48efbc434482db5a5b67112
saurav526 <sauravtiwari2712005@gmail.com>
2026-06-20 21:04:41 +0530

    local hugging face

---
+++ Chatmodels/hugginf face local.py
@@ -0,0 +1,17 @@
+from langchain_huggingface import HuggingFacePipeline
+from dotenv import load_dotenv
+
+
+load_dotenv()  # Load environment variables from .env file
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs={
+        "max_length": 100,
+        "temperature": 0.9
+    }
+)
+
+response = llm.invoke("What is Artificial Intelligence?")
+print(response)
