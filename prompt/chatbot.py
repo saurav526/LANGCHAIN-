ba81df81494d58b2a66b5eccd51dbc56177cc51b
saurav526 <sauravtiwari2712005@gmail.com>
2026-07-08 14:03:48 +0530

    Adding message in the chatbot

--- prompt/chatbot.py
+++ prompt/chatbot.py
@@ -1,20 +1,26 @@
 from langchain_OpenAI import ChatOpenAI
 from dotenv import load_dotenv
+from langchain.schema import HumanMessage, AIMessage ,SystemMessage
+
 
 load_dotenv()
 model= ChatOpenAI()
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="Hi, how are you?"),
+    AIMessage(content="I'm good, thanks! How can I help you today?")
+]
 
 while True:
     user_input = input("User: ")
-    chat_history.append(("user", user_input))
+    chat_history.append(HumanMessage(content=user_input))
     if user_input.lower() == "exit":
         break
 
     prompt = f"User: {user_input}\nAI:"
     response = model.invoke(chat_history)
-    chat_history.append(("ai", response.content))
+    chat_history.append(AIMessage(content=response.content))
     print(f"AI: {response.content}")
 
 print(chat_history)
