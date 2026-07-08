da810c859c5989c7d8da66ee5f7a44e828af9494
saurav526 <sauravtiwari2712005@gmail.com>
2026-07-08 13:52:50 +0530

    add chat history

--- prompt/chatbot.py
+++ prompt/chatbot.py
@@ -4,11 +4,17 @@ from dotenv import load_dotenv
 load_dotenv()
 model= ChatOpenAI()
 
+chat_history = []
+
 while True:
     user_input = input("User: ")
+    chat_history.append(("user", user_input))
     if user_input.lower() == "exit":
         break
 
     prompt = f"User: {user_input}\nAI:"
-    response = model.invoke(prompt)
+    response = model.invoke(chat_history)
+    chat_history.append(("ai", response.content))
     print(f"AI: {response.content}")
+
+print(chat_history)
