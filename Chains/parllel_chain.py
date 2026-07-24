d397f102c70e8e9ac1fa0ad20e4ec44da091d738
Saurav kumar <sauravtiwari2712005@gmail.com>
2026-07-24 21:20:24 +0530

    Fix formatting and import issues in parallel_chain.py

--- Chains/parllel_chain.py
+++ Chains/parllel_chain.py
@@ -1,7 +1,7 @@
 from langchain_openai import OpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from lngchain_core.output_parser import StrOutputParser
+from langchain_core.output_parser import StrOutputParser
 from langchain_anthropic import ChatAnthropic
 from langchain.schema.runnable import RunnableParallel
 
